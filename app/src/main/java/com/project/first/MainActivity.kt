ce9e8516619a6e56a154d69b6a0db73a506be6b0
lyc <lyc@hzlpjywwgc.onexmail.com>
2020-11-26 23:00:50 +0800

    test

--- app/src/main/java/com/project/first/MainActivity.kt
+++ app/src/main/java/com/project/first/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /**
+         * git remote add origin git@git
+         * git push -u origin master
+         * git branch --set-upstream-to=origin/v1.1.0 v1.1.0
+         */
     }
 }
